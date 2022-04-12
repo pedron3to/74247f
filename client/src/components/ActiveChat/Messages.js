23463a1f8c69c94e132ef6e66a96aeb2ff4a2415
pedron3to <pedro_neto@outlook.com>
2022-04-11 21:22:19 -0300

    Remove reverse function from ActiveChat

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Box } from '@material-ui/core';
-import { SenderBubble, OtherUserBubble } from '.';
-import moment from 'moment';
+import React from "react";
+import { Box } from "@material-ui/core";
+import { SenderBubble, OtherUserBubble } from ".";
+import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
   return (
     <Box>
-      {messages.reverse().map((message) => {
-        const time = moment(message.createdAt).format('h:mm');
+      {messages.map((message) => {
+        const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} />
