a34c784f30789e9fb8a32a8b2bc4ee964780d523
pedron3to <pedro_neto@outlook.com>
2022-04-13 01:00:02 -0300

    Add layout to Signup component

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,17 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import {
-  Grid,
-  Box,
-  Typography,
-  Button,
-  FormControl,
-  TextField,
-  FormHelperText,
-} from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import { Box, Typography } from "@material-ui/core";
+
+import Layout from "./components/Layout";
+import Button from "./components/Button";
+import InputForm from "./components/InputForm";
+
+const useStyles = makeStyles((theme) => ({
+  contentContainer: {
+    width: 420,
+    marginLeft: "auto",
+    marginRight: "auto",
+    [theme.breakpoints.down("sm")]: {
+      width: "fit-content",
+    },
+  },
+  createAccountWrapper: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    marginTop: 30,
+    [theme.breakpoints.down("sm")]: {
+      justifyContent: "center",
+    },
+  },
+  subtitle1: {
+    marginRight: 30,
+  },
+  signUpFormWrapper: {
+    width: 380,
+    marginTop: "10vh",
+    alignItems: "center",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+      width: "fit-content",
+      padding: 16,
+    },
+  },
+  buttonWrapper: {
+    display: "flex",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+  },
+}));
 
 const Signup = ({ user, register }) => {
   const history = useHistory();
+  const classes = useStyles();
 
   const [formErrorMessage, setFormErrorMessage] = useState({});
 
@@ -25,86 +63,58 @@ const Signup = ({ user, register }) => {
     const confirmPassword = formElements.confirmPassword.value;
 
     if (password !== confirmPassword) {
-      setFormErrorMessage({ confirmPassword: 'Passwords must match' });
+      setFormErrorMessage({ confirmPassword: "Passwords must match" });
       return;
     }
     await register({ username, email, password });
   };
 
   useEffect(() => {
-    if (user && user.id) history.push('/home');
+    if (user && user.id) history.push("/home");
   }, [user, history]);
 
   return (
-    <Grid container justifyContent="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Link href="/login" to="/login">
-            <Button>Login</Button>
+    <Layout>
+      <Box className={classes.contentContainer}>
+        <Box className={classes.createAccountWrapper}>
+          <Typography className={classes.subtitle1} variant='subtitle1'>
+            Need to log in?
+          </Typography>
+          <Link href='/login' to='/login'>
+            <Button variant='secondarySignup' title='Login' />
           </Link>
-        </Grid>
+        </Box>
         <form onSubmit={handleRegister}>
-          <Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                  required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  label="E-mail address"
-                  aria-label="e-mail address"
-                  type="email"
-                  name="email"
-                  required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  aria-label="password"
-                  label="Password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="password"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  label="Confirm Password"
-                  aria-label="confirm password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="confirmPassword"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Button type="submit" variant="contained" size="large">
-              Create
-            </Button>
-          </Grid>
+          <Box className={classes.signUpFormWrapper}>
+            <Typography variant='h1'>Create an account.</Typography>
+
+            <InputForm label='Username' name='username' type='text' />
+
+            <InputForm label='E-mail address' name='email' type='email' />
+
+            <InputForm
+              label='Password'
+              name='password'
+              type='password'
+              inputProps={{ minLength: 6 }}
+              error={formErrorMessage.confirmPassword}
+            />
+
+            <InputForm
+              label='Confirm Password'
+              name='confirmPassword'
+              type='password'
+              inputProps={{ minLength: 6 }}
+              error={formErrorMessage.confirmPassword}
+            />
+
+            <Box className={classes.buttonWrapper}>
+              <Button type='primary' variant='primary' title='Create' />
+            </Box>
+          </Box>
         </form>
       </Box>
-    </Grid>
+    </Layout>
   );
 };
 
