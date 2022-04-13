730db635009585611af10ef54bf0143933edfe16
pedron3to <pedro_neto@outlook.com>
2022-04-13 01:02:28 -0300

    Add new values to mui theme

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -7,18 +7,33 @@ export const theme = createMuiTheme({
     button: {
       textTransform: "none",
       letterSpacing: 0,
-      fontWeight: "bold"
-    }
+      fontWeight: "bold",
+    },
+    subtitle1: {
+      fontSize: 14,
+      color: "#B0B0B0",
+      lineHeight: "19px",
+      fontWeight: 400,
+    },
+    h1: {
+      fontSize: 26,
+      color: "#000",
+      lineHeight: "40px",
+      fontWeight: 600,
+    },
   },
   overrides: {
     MuiInput: {
       input: {
-        fontWeight: "bold"
-      }
-    }
+        fontWeight: "bold",
+        paddingBottom: 6,
+        fontSize: 14,
+        lineHeight: 19,
+      },
+    },
   },
   palette: {
     primary: { main: "#3A8DFF" },
-    secondary: { main: "#B0B0B0" }
-  }
+    secondary: { main: "#B0B0B0" },
+  },
 });
