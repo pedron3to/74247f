7380a360c4a2ac6556514ba5cb49cba87f313115
pedron3to <pedro_neto@outlook.com>
2022-04-15 00:43:51 -0300

    Fix overlay SideBanner

--- client/src/components/SideBanner/index.js
+++ client/src/components/SideBanner/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import PeopleImage from "./assets/bg-img.png";
 import Bubble from "./assets/bubble.svg";
 
@@ -7,20 +7,36 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
   root: {
+    position: "absolute",
+    height: "100vh",
+    width: 425,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    mixBlendMode: "normal",
+    backgroundImage: `url(${PeopleImage})`,
+  },
+  overlay: {
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundImage: "linear-gradient(180deg, #3A8DFF 0%, #86B9FF 85% )",
+    height: "100vh",
+    width: 425,
+    opacity: 0.85,
+  },
+  content: {
+    position: "absolute",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     height: "100vh",
-    width: "41.5vw",
-    backgroundImage: `linear-gradient(180deg, rgba(58, 141, 255, 0.9), rgba(134, 185, 255, 0)  ), url(${PeopleImage})`,
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    mixBlendMode: "normal",
-    OpacityTwoTone: 0.85,
+    width: 425,
+    opacity: 1,
   },
   bubble: {
     width: 66,
-    marginTop: "28vh",
+    marginTop: 199.5,
   },
   slogan: {
     width: 269,
@@ -36,17 +52,22 @@ const SideBanner = () => {
   const classes = useStyles();
 
   return (
-    <Box className={classes.root}>
-      <img
-        className={classes.bubble}
-        src={Bubble}
-        alt='bubble'
-        aria-label='bubble image'
-      />
-      <Typography className={classes.slogan}>
-        Converse with anyone with any language
-      </Typography>
-    </Box>
+    <>
+      <div className={classes.root} />
+      <div className={classes.overlay} />
+
+      <div className={classes.content}>
+        <img
+          className={classes.bubble}
+          src={Bubble}
+          alt='bubble'
+          aria-label='bubble image'
+        />
+        <Typography className={classes.slogan}>
+          Converse with anyone with any language
+        </Typography>
+      </div>
+    </>
   );
 };
 
