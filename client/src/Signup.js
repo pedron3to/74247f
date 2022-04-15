651447192967842ee82593e2e3ee4c625a7c697a
pedron3to <pedro_neto@outlook.com>
2022-04-15 00:38:50 -0300

    Refactor signup page to adhere DRY principle

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,55 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import { Box, Typography } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
-import Layout from "./components/Layout";
 import Button from "./components/Button";
+import ButtonWrapper from "./components/ButtonWrapper";
+import Form from "./components/Form";
+import HeaderSignupLogin from "./components/HeaderSignupLogin";
 import InputForm from "./components/InputForm";
-
-const useStyles = makeStyles((theme) => ({
-  contentContainer: {
-    width: 420,
-    marginLeft: "auto",
-    marginRight: "auto",
-    [theme.breakpoints.down("sm")]: {
-      width: "fit-content",
-    },
-  },
-  createAccountWrapper: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    marginTop: 30,
-    [theme.breakpoints.down("sm")]: {
-      justifyContent: "center",
-    },
-  },
-  subtitle1: {
-    marginRight: 30,
-  },
-  signUpFormWrapper: {
-    width: 380,
-    marginTop: "10vh",
-    alignItems: "center",
-    [theme.breakpoints.down("sm")]: {
-      flexDirection: "column",
-      width: "fit-content",
-      padding: 16,
-    },
-  },
-  buttonWrapper: {
-    display: "flex",
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-  },
-}));
+import Layout from "./components/Layout";
 
 const Signup = ({ user, register }) => {
   const history = useHistory();
-  const classes = useStyles();
 
   const [formErrorMessage, setFormErrorMessage] = useState({});
 
@@ -75,45 +35,37 @@ const Signup = ({ user, register }) => {
 
   return (
     <Layout>
-      <Box className={classes.contentContainer}>
-        <Box className={classes.createAccountWrapper}>
-          <Typography className={classes.subtitle1} variant='subtitle1'>
-            Need to log in?
-          </Typography>
-          <Link href='/login' to='/login'>
-            <Button variant='secondary' title='Login' />
-          </Link>
-        </Box>
-        <form onSubmit={handleRegister}>
-          <Box className={classes.signUpFormWrapper}>
-            <Typography variant='h1'>Create an account.</Typography>
-
-            <InputForm label='Username' name='username' type='text' />
-
-            <InputForm label='E-mail address' name='email' type='email' />
-
-            <InputForm
-              label='Password'
-              name='password'
-              type='password'
-              inputProps={{ minLength: 6 }}
-              error={formErrorMessage.confirmPassword}
-            />
-
-            <InputForm
-              label='Confirm Password'
-              name='confirmPassword'
-              type='password'
-              inputProps={{ minLength: 6 }}
-              error={formErrorMessage.confirmPassword}
-            />
-
-            <Box className={classes.buttonWrapper}>
-              <Button type='primary' variant='primary' title='Create' />
-            </Box>
-          </Box>
-        </form>
-      </Box>
+      <HeaderSignupLogin
+        title='Already have an account?'
+        href='/login'
+        titleButton='Login'
+        variantButton='secondarySignup'
+      />
+      <Form
+        handleSubmit={handleRegister}
+        title='Create an account.'
+        marginTop={10}
+      >
+        <InputForm label='Username' name='username' type='text' />
+        <InputForm label='E-mail address' name='email' type='email' />
+        <InputForm
+          label='Password'
+          name='password'
+          type='password'
+          inputProps={{ minLength: 6 }}
+          error={formErrorMessage.confirmPassword}
+        />
+        <InputForm
+          label='Confirm Password'
+          name='confirmPassword'
+          type='password'
+          inputProps={{ minLength: 6 }}
+          error={formErrorMessage.confirmPassword}
+        />
+        <ButtonWrapper marginTop={25}>
+          <Button type='primary' variant='primary' title='Create' />
+        </ButtonWrapper>
+      </Form>
     </Layout>
   );
 };
